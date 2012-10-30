ec34ebb975fb2459a9cad303034268abbf8b5530
Nick Weil <speedquest@fullsail.edu>
2012-10-30 15:44:41 -0400

    Inital Commit.  Have the base Twitter program NSLogging tweets and Twitter information.  Added a TableView to the ViewController.xib.

---
+++ MDF2/MDF2_Twitter_Week2/MDF2_Week2/MDF2_Week2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MDF2_Week2
+//
+//  Created by Nicholas Weil on 10/30/12.
+//  Copyright (c) 2012 Nicholas Weil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ MDF2/MDF2_Twitter_Week2/MDF2_Week2/MDF2_Week2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MDF2_Week2
+//
+//  Created by Nicholas Weil on 10/30/12.
+//  Copyright (c) 2012 Nicholas Weil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
